8a583fd55f441ec2ed7afe4ddd1a3ef915f07b17
Manuel <manuel.nagel@student.uni-tuebingen.de>
2021-06-25 20:01:22 +0200

    update

--- NuScenesLoader.py
+++ NuScenesLoader.py
@@ -13,41 +13,40 @@ import h5py
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 rel_data_path = '/data/sets/nuscenes'
 dataroot = BASE_DIR + rel_data_path
-
-# Test code with mini dataset
-mini_testrun = True
+print(dataroot)
 
 
 # references: https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
 #             https://github.com/erikwijmans/Pointnet2_PyTorch/tree/master/pointnet2/data
 
 class NuScenesLoader(data.Dataset):
-    def __init__(self, num_points, root=dataroot, transforms=None, train=True, dataset=None):
+    def __init__(self, num_points, root=dataroot, transforms=None, train=True, mini_testrun=True):
         super().__init__()
         self.root = root
         self.transforms = transforms
         self.num_points = num_points
         if mini_testrun:
-            self.dataset = NuScenes(version='v1.0-mini', dataroot=self.root, verbose=True)
+            self.dataset = NuScenes(version='v1.0-mini', dataroot=dataroot, verbose=True)
+            if train:
+                self.boxes = load_gt(nusc=self.dataset, eval_split='mini_train', box_cls=DetectionBox, verbose=True)
         else:
-            #TODO prepare train and test datasets here
+            #TODO load train and test datasets here
             pass
 
     def __getitem__(self, idx):
-        annotation = self.dataset.sample_annotation[idx]
-        print(annotation)
-        sample = self.dataset.get('sample', annotation['sample_token'])
-        print(sample)
-        if annotation['num_lidar_pts'] > 0:
-            pass
-        if annotation['num_radar_pts'] > 0:
-            pass
+        return self.boxes.all[idx] # TODO delete this later
 
-        return annotation
+        # TODO get partial pcd (filter for points in box)
+        box = self.boxes[idx]
+        sample_data = self.dataset.get('sample', box.sample_token)['data']
+        #points =
+        pcd = filter_points(box, points)
+        label = box.detection_name
+        return pcd, label
 
-    # TODO Return number of annotations
+    # TODO Return number of annotations (boxes)
     def __len__(self):
-        return len(self.dataset.sample_annotation)
+        return len(self.boxes.all)
 
     def set_num_points(self, pts):
         self.num_points = min(int(1e4), pts)
@@ -55,7 +54,4 @@ class NuScenesLoader(data.Dataset):
 
 dataset = NuScenesLoader(16, train=True)
 
-#print(dataset[46])
-boxes = load_gt(nusc=dataset.dataset, eval_split='mini_train', box_cls=DetectionBox, verbose=True)
-for b in boxes:
-    print(b)
+print(dataset.boxes.__repr__)
